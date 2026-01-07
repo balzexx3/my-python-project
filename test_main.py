2eb1d40c49dbed3a1b8f4d3ecd019d17902ea6e4
AzdoudTime <hemza1@gmail.com>
2026-01-07 15:33:26 +0100

    Fix indentation in pytest test

--- test_main.py
+++ test_main.py
@@ -1,11 +1,5 @@
 from main import add
 def test_add():
-assert add(3, 4) == 7
-assert add(-1, 1) == 0
-assert add(0, 0) == 0
-4. Initialisez votre dépôt Git :
-git init
-git add .
-git commit -m "Initial commit"
-git remote add origin <URL_DE_VOTRE_REPOSITORY>
-git push -u origin master
+	assert add(3, 4) == 7
+	assert add(-1, 1) == 0
+	assert add(0, 0) == 0
